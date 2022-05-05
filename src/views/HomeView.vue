7a52885201d8b3b35d4c11962e8c9bfb18d4701f
jason <jduong89@gmail.com>
2022-05-04 17:19:13 -0700

    adds add to library button and function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,6 +20,20 @@ export default {
       console.log(this.previousPage);
     });
   },
+  methods: {
+    addLibrary: function (game) {
+      (this.currentGame.game_id = game.id),
+        (this.currentGame.title = game.name),
+        (this.currentGame.image = game.background_image),
+        console.log("new entry", this.currentGame);
+      axios
+        .post("http://localhost:3000/libraries", this.currentGame)
+        .then((response) => {
+          console.log("Success", response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
+  },
 };
 </script>
 
@@ -29,7 +43,9 @@ export default {
     <img :src="game.background_image" v-bind:alt="game.name" class="artwork" />
     <h3>{{ game.name }}</h3>
     <a v-bind:href="`/games/${game.id}`" class="btn btn-primary">More info</a>
+    <br />
     <!-- button to add to library when logged in -->
+    <button v-on:click="addLibrary(game)">Add to library</button>
   </div>
 </template>
 
