6884b382fad9a01150986a5b5fd64a991144f632
jason <jduong89@gmail.com>
2022-05-09 21:03:25 -0700

    removes about page

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,6 @@ export default {
     <router-link to="/">Home</router-link>
     |
     <router-link to="/search">Search</router-link>
-    |
-    <router-link to="/about">About</router-link>
     <span v-if="!isLoggedIn">
       |
       <router-link to="/signup">Signup</router-link>
@@ -35,25 +33,4 @@ export default {
   <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
